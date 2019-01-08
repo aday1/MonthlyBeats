9a08f6a016a299b13f4802d6babc4efe6820aeaf
Aday <aday@aday.net.au>
2019-01-08 20:56:24 +1100

    Includes
    Header/Footer etc

---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+
+  </div>  <!-- content -->
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Monthlybeats</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Tangerine'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Plaster'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Plaster|Poiret+One'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+
+<style>
+body {
+background-color: #80aadc
+
+}
+</style>
+
+
+
+<container class="container">
+  
+<header>
+</header>
+  
+  <section>
+    
+  </section>
+  
+<main class="main-body">
+  <table class="counter-style">
+
+
+  <tr class="timer">
+    <td id="days">1</td>
+    <td id="hours">30</td>
+    <td id="minutes">25</td>
+    <td id="seconds">60</td>
+
+<style>
+body {
+background-color: #ffd1da
+
+}
+</style>
+
+
+  </tr>
+
+
+  <tr class="text">
+    <td>Days</td>
+    <td>Hours</td>
+    <td>Minutes</td>
+    <td class="text-seconds">Seconds</td>
+
+  </tr>
+
+</table>
+  </main>
+</container>
+  
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
+
+
+
+<!--- content -->
+
+
+
+
+
+
+
+
+
+  <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="//cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/uikit/2.18.0/js/uikit.min.js"></script>
+  <script src="/js/myapp.js"></script>
+
+
+
+
+</body>
+</html>

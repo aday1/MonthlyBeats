5c53ca70935c8396f18626e786341b6e6560fe4d
Aday <aday@aday.net.au>
2019-01-08 20:55:31 +1100

    Classes Folder
    Configs etc here

---
+++ classes/Config.class.php
@@ -0,0 +1,18 @@
+<?php
+ 
+/**
+ * Configuration class
+ */
+
+class Config
+{
+
+  const DB_HOST = 'localhost',
+        DB_NAME = 'monthlybeats',
+        DB_USER = 'admin',
+        DB_PASS = 'NAAAAAAAAAAAAAAAAAHHHH',
+        SMTP_HOST = 'smtp.gmail.com',
+        SMTP_USER = 'servers@aday.net.au',
+        SMTP_PASS = 'NAHHHHHHHH';
+
+}

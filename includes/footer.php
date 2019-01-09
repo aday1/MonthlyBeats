f5aecc851cf1945381c04b7545f69982b4166210
Aday <aday@aday.net.au>
2019-01-09 11:16:05 +1100

    FAVICON Enable

--- includes/footer.php
+++ includes/footer.php
@@ -16,6 +16,10 @@
 
       <link rel="stylesheet" href="css/style.css">
 
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+  
   
 </head>
 
